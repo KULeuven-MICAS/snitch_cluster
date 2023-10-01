bb538e4c65a258f8b822b7ebaa8ac0530ef4f0e8
rgantonio <ryan.antonio@esat.kuleuven.be>
2023-10-01 18:44:34 +0200

    lint: Add license

--- target/snitch_cluster/sw/apps/snax-gemm-engine/src/snax-gemm-engine.c
+++ target/snitch_cluster/sw/apps/snax-gemm-engine/src/snax-gemm-engine.c
@@ -1,4 +1,8 @@
 
+// Copyright 2020 ETH Zurich and University of Bologna.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
 #include "data.h"
 #include "snrt.h"
 
